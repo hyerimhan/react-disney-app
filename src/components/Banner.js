ccef37e17ebea3d6936cf3c1736825d7dae98083
hyerimhan <hnhyerim@gmail.com>
2022-12-21 22:00:26 +0900

    feat: modified Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import axios from '../api/axios'
+import { useEffect } from 'react'
+import requests from '../api/requests'
 
 function Banner() {
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const response = await axios.get(requests.fetchNowPlaying)
+    console.log(response)
+  }
+
   return (
     <div>
-      
+      Banner
     </div>
   )
 }
