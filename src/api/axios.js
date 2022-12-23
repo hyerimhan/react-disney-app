87b5e66a286039b971f944a1cb88a177ab5f9d03
hyerimhan <hnhyerim@gmail.com>
2022-12-23 20:51:19 +0900

    feat: hide API KEY

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-// const {API_KEY} = process.env.REACT_APP_API_KEY
-
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "e32fa88f79896fd0a278db5b42dfdf2b",
+    api_key: process.env.REACT_APP_API_KEY,
     language: "ko-KR",
   },
 });
