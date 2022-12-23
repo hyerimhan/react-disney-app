56701cba882319c6750703f75d37cbf0c3a670f8
hyerimhan <hnhyerim@gmail.com>
2022-12-23 21:54:10 +0900

    feat: complete Category & ongoing Row

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import axios from '../api/axios';
+import React, { useEffect, useState } from 'react'
 
-const Row = () => {
+
+const Row = ({title, id, fetchUrl}) => {
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    fetchMovieData();
+  }, [])
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+  }
+
   return (
     <div>
-      
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className='arrow'>
+            {"<"}
+          </span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className="row__poster"
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt={movie.name}
+              // onClick={() => handleClick(movie)}
+            />
+          ))}
+        </div>
+        <div className='slider__arrow-right'>
+          <span className="arrow">
+            {">"}
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
