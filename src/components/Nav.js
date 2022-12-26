f735813e7b5b68057a8e29eb9735d5ece16066d9
hyerimhan <hnhyerim@gmail.com>
2022-12-26 21:57:10 +0900

    feat: searchPage Route

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Nav = () => {
 
   const [show, setShow] = useState(false);
+  const {pathname} = useLocation()
+  const [searchValue, setSearchValue] = useState("")
+  const navigate = useNavigate();
+
   useEffect(() => {
     window.addEventListener('scroll', () => {
       if(window.scrollY > 50) {
@@ -18,14 +23,57 @@ const Nav = () => {
     }
   }, [])
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value)
+    navigate(`/search?q=${e.target.value}`)
+  } 
+
   return (
     <NavWrapper show={show}>
       <Logo>
         <img alt="Disney Plus logo" src="/images/logo.svg" onClick={() => (window.location.href = "/")}/>
       </Logo>
+      {pathname === '/' ? (
+        <Login>Login</Login>
+        )  :
+        <Input 
+          value={searchValue}
+          onChange={handleChange}
+          className="nav__input"
+          type='text'
+          placeholder='영화를 검색해주세요.'
+        />
+      }
     </NavWrapper>
   )
 }
+
+const Login = styled.a`
+  background-color: rgba(0,0,0,0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing:1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`
+
+const Input = styled.input`
+  position:fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(0,0,0,0.582);
+  border-radius: 5px;
+  color:#fff;
+  padding:5px;
+  border:none;
+`
 
 const NavWrapper = styled.nav`
   position:fixed;
